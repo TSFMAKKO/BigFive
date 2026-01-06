7fd22bb2688cc708810e1d65a7608f1bf246e7e0
tsfmakko <racingf1@gmail.com>
2026-01-06 15:56:45 +0800

    feat: add result and test pages for personality assessment
    - Created ResultPage component to display test results with navigation links for different personality traits.
    - Implemented StartTestPage component to initiate the personality test.
    - Developed TestPage component to show the ongoing test status and provide a link to calculate results.
    - Added individual result components for each personality trait: Agreeableness, Conscientiousness, Extroversion, Neuroticism, and Openness, each with navigation links to other traits.

---
+++ src/pages/StartTestPage.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export default function StartTestPage() {
+  return (
+    <div className="">
+      <div className="">
+        <Link to="/test">開始測驗</Link>
+      </div>
+    </div>
+  );
+}
