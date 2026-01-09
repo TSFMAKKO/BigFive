60b7e6f7242804b935490836a61037d41add5ac0
tsfmakko <racingf1@gmail.com>
2026-01-09 11:43:27 +0800

    feat: pass resData to TestPage and implement question processing logic

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,6 +1,33 @@
+import { useEffect, useMemo } from "react";
 import { Link } from "react-router-dom";
 
-export default function TestPage() {
+export default function TestPage({ resData }) {
+  const questions = useMemo(() => {
+    const problemList = resData?.problemList ?? {};
+    return Object.entries(problemList).reduce((acc, [traitKey, traitData]) => {
+      const traitQuestions = (traitData?.problems ?? []).reduce(
+        (questionAcc, problem) => {
+          questionAcc[problem.problem] = problem.options.reduce(
+            (optionAcc, option) => {
+              optionAcc[option.description] = option.fraction;
+              return optionAcc;
+            },
+            {}
+          );
+          return questionAcc;
+        },
+        {}
+      );
+      acc[traitKey] = traitQuestions;
+      return acc;
+    }, {});
+  }, [resData]);
+
+  useEffect(() => {
+    console.log("questions:", questions);
+  }, [questions]);
+
+ 
   return (
     <div className="">
       <div className="">
